e86c655534e279f911c12cb336c84585c52c93f0
Madhav <madhavadepu@gmail.com>
2018-08-04 19:19:32 -0700

    adding initial version

--- src/com/mirketa/PivotalTracker.groovy
+++ src/com/mirketa/PivotalTracker.groovy
@@ -3,9 +3,11 @@ package com.mirketa;
 
 class PivotalTracker implements Serializable  {
   def apiToken
+  @NonCPS
   PivotalTracker(token){
   	this.apiToken=token
   }
+  @NonCPS
   void getToken(){
   	println apiToken
   }
