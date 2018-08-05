e12ef23ce71f30b63eb70ab1e445dd6a1c66c577
Madhav <madhavadepu@gmail.com>
2018-08-04 19:37:31 -0700

    adding initial version

--- src/com/mirketa/PT.groovy
+++ src/com/mirketa/PT.groovy
@@ -3,11 +3,11 @@ package com.mirketa;
 
 class PT implements Serializable  {
   def apiToken
-  @NonCPS
+
   PT(token){
   	this.apiToken=token
   }
-  @NonCPS
+
   void getToken(){
   	println apiToken
   }
