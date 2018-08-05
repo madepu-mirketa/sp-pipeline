b862da46bf245027de7734eeb2afd7536a9f8d74
Madhav <madhavadepu@gmail.com>
2018-08-05 07:59:03 -0700

    Adding getStories with credentials function.

--- src/com/mirketa/PT.groovy
+++ src/com/mirketa/PT.groovy
@@ -1,15 +1,57 @@
 package com.mirketa;
 
+import groovy.json.JsonSlurper
+
 
 class PT{
-  def apiToken
+    def apiToken=''
 
-  PT(token){
-  	apiToken=token
-  }
+    PT(){
+        withCredentials([string(credentialsId: 'pt_api_token', variable: 'pt_api_token')]) {
+            apiToken = pt_api_token
+        }
+    }
 
-  def getToken(){
-  	println apiToken
-  	return apiToken
-  }
+    def getStories(pt_project_id,pt_state='',pt_label='',pt_update_after=''){
+        if(apiToken.equals("")){
+            println "Please set Jenkins credentials with pt_api_token"
+            retun
+        }
+        def storyIds, labels, branches
+
+        def storiesPathTemplate = "projects/%s/stories"
+        def storiesLabelPathTemplate = "projects/%s/stories/%s/labels"
+        def storyPathTemplate = "projects/%s/stories/%s"
+
+        def base ="https://www.pivotaltracker.com/services/v5/"
+
+        def storiesPath=String.format(storiesPathTemplate,pt_project_id)
+
+        def query='?fields=current_state%2Clabels%2Cbranches%2Ctasks&date_format=millis'
+        if(!pt_state.equals("")){
+            query=query+'&with_state='+pt_state
+        }
+        if(!pt_update_after.equals("")){
+            query=query+'&updated_after='+pt_update_after
+        }
+        if(!pt_label.equals("")){
+            query=query+'&with_label='+pt_label
+        }
+
+        def pt_url=base + storiesPath + query
+        def headers=[maskValue: true, name: 'X-TrackerToken', value: apiToken]
+
+        def response = httpRequest acceptType: 'APPLICATION_JSON', contentType: 'APPLICATION_JSON', customHeaders: [headers], responseHandle: 'LEAVE_OPEN', url: pt_url
+        //println('Status: '+response.status)
+        //println('Response: '+response.content)
+
+        def json = new JsonSlurper().parseText(response.content)
+        storyIds=json*.id
+        println "storyIds: "+ storyIds
+        labels=json*.labels.name
+        println "labels: "+labels
+        branches=json*.branches.name
+        println "branches: "+branches
+        return [storyIds,labels,branches]
+    }
 }
